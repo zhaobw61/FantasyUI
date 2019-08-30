cfb11bd6cfa9ac4c770de4a9b1fd8f8379eda056
zhaobw <1964471757@qq.com>
2019-08-30 17:27:45 +0800

    填写icon和iconposition

--- src/button.vue
+++ src/button.vue
@@ -1,9 +1,18 @@
 <template>
-  <button class="g-button">按钮</button>
+  <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
+    <svg class="icon" v-if="icon">
+      <use :xlink:href="`#i-${icon}`"></use>
+    </svg>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 
 <script>
-export default {};
+export default {
+  props:['icon','iconPosition']
+};
 </script>
 
 <style lang='scss'>
@@ -14,6 +23,10 @@ export default {};
   border-radius: var(--border-radius);
   border: 1px solid var(--border-color);
   background: var(--button-bg);
+  display:inline-flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align: middle;
   &:hover {
     border-color: var(--border-color-hover);
   }
@@ -24,6 +37,23 @@ export default {};
 
   &:focus {
     outline: none;
+  }
+  > .content{
+    order:2;
+  }
+  > .icon {
+     order:1;
+     margin-right: .3em;
+  }
+  &.icon-right{
+    > .content{
+      order:1;
+    }
+    > .icon{
+      order:2;
+      margin-right: 0;
+      margin-left: .3em;
+    }
   }
 }
 </style>
