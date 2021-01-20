9e0a0e17a2fa9a2975192fd0a1054f9b52657de1
zhaobowen <bowen.zhao@chiefclouds.com>
2021-01-20 13:59:49 +0800

    实现position的功能

--- src/toast.vue
+++ src/toast.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="toast" ref="wrapper">
+    <div class="toast" ref="wrapper" :class="toastClasses">
         <div class="message">
             <slot v-if="!enableHtml"></slot>
             <div v-else v-html="$slots.default[0]"></div>
@@ -33,11 +33,25 @@ export default {
         enableHtml: {
             type: Boolean,
             default:false
+        },
+        position: {
+            type: String,
+            default: 'top',
+            validator(value) {
+                return ['top', 'bottom', 'middle'].indexOf(value) >= 0;
+            }
         }
     },
     mounted () {
         this.execAutoClose();
         this.updateStyles();
+    },
+    computed: {
+        toastClasses(){
+            return {
+                [`position-${this.position}`]: true
+            }
+        }
     },
     methods: {
         updateStyles() {
@@ -73,24 +87,37 @@ export default {
     $toast-bg: rgba(0, 0, 0, 0.75);
     .toast {
         font-size: $font-size;min-height: $toast-min-height; line-height: 1.8;
-        position: fixed;top: 0; left: 50%;transform: translateX(-50%);display: flex;
+        position: fixed;display: flex;
         color: white;
         align-items: center;
         background: $toast-bg;
         border-radius: 4px;
         box-shadow: 0 0 3px 0 rbga(0, 0, 0, 0.5);
         padding: 0 16px;
+        left: 50%;
         .message {
             padding: 4px 0;
         }
-    }
-    .close {
-        padding-left: 16px;
-    }
-    .line {
-        height: 100%;
-        border-left: 1px solid #666;
-        margin-left: 16px;
+        .close {
+            padding-left: 16px;
+        }
+        .line {
+            height: 100%;
+            border-left: 1px solid #666;
+            margin-left: 16px;
+        }
+        &.position-top {
+            top: 0;
+            transform: translateX(-50%);
+        }
+        &.position-bottom {
+            bottom: 0;
+            transform: translateX(-50%);
+        }
+        &.position-middle {
+            top: 50%;
+            transform: translateX(-50%, -50%);
+        }
     }
 </style>
 
