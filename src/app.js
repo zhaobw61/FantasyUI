228465e6c4d9ba288cb800d6ca835ccd7816840e
zhaobowen <bowen.zhao@chiefclouds.com>
2021-01-25 15:28:06 +0800

    初始化popover

--- src/app.js
+++ src/app.js
@@ -13,6 +13,7 @@ import TabsHead from './tabs-head'
 import TabsBody from './tabs-body'
 import TabsItem from './tabs-item'
 import TabsPane from './tabs-pane'
+import Popover from './popover'
 
 
 
@@ -29,6 +30,7 @@ Vue.component('g-tabs-head', TabsHead)
 Vue.component('g-tabs-body', TabsBody)
 Vue.component('g-tabs-item', TabsItem)
 Vue.component('g-tabs-pane', TabsPane)
+Vue.component('g-popover', Popover)
 
 Vue.use(plugin)
 
