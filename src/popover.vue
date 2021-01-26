56247ac3641633a0303eabd0f25f47bf87ef94e3
zhaobowen <bowen.zhao@chiefclouds.com>
2021-01-26 13:53:41 +0800

    解决overflow的bug

--- src/popover.vue
+++ src/popover.vue
@@ -1,9 +1,11 @@
 <template>
-    <div class="popover" @click="Onclick">
-        <div class="content-wrapper" v-if="visible">
+    <div class="popover" @click.stop="Onclick">
+        <div ref="contentWrapper" class="content-wrapper" v-show="visible" @click.stop >
             <slot name="content"></slot>
         </div>
-        <slot></slot>
+        <span ref="triggerWrapper">
+            <slot></slot>
+        </span>
     </div>
 </template>
 <script>
@@ -17,21 +19,32 @@ export default {
     methods: {
         Onclick() {
             this.visible = !this.visible;
+            if (this.visible === true) {
+                document.body.appendChild(this.$refs.contentWrapper);
+                let {width, height, top, left} = this.$refs.triggerWrapper.getBoundingClientRect();
+                this.$refs.contentWrapper.style.left = left + window.scrollX + 'px';
+                this.$refs.contentWrapper.style.top = top + window.scrollY + 'px';
+                let eventHandler = () => {
+                    this.visible = false;
+                    document.removeEventListener('click', eventHandler);
+                }
+                document.addEventListener('click', eventHandler)
+            }
         }
     }
 }
 </script>
 <style scoped lang="scss">
     .popover {
+        margin: 50px;
         display: inline-block;
         vertical-align: top;
         position: relative;
-        .content-wrapper {
-            position: absolute;
-            bottom: 100%;
-            left: 0;
-            border: 1px solid red;
-            box-shadow: 0 0 3px rgba(0,0,0,0.5);
-        }
+    }
+    .content-wrapper {
+        position: absolute;
+        border: 1px solid red;
+        box-shadow: 0 0 3px rgba(0,0,0,0.5);
+        transform: translateY(-100%)
     }
 </style>
