0b3939191a76056cb67d72953eb210d9d1856edc
zhaobowen <bowen.zhao@chiefclouds.com>
2021-02-24 16:39:05 +0800

    导入数据库

--- src/demo.vue
+++ src/demo.vue
@@ -1,12 +1,15 @@
 <template>
     <div>
         <g-cascader :source="source"
-            :selected="selected" @update:selected="selected = $event"></g-cascader>
+            :selected.sync="selected"></g-cascader>
     </div>
 </template>
 <script>
 import Cascader from './cascader'
-// import Icon from './icon'
+import db from './db'
+function ajax(parent_id = 0) {
+    return db.filter(item => item.parent_id == parent_id);
+}
 export default {
     name:'demo',
     components: {
@@ -15,46 +18,7 @@ export default {
     data() {
         return {
             selected:[],
-            source: [
-                {
-                    name: '浙江',
-                    children: [
-                    {
-                        name: '杭州',
-                        children: [
-                        {name: '杭州-1'},
-                        {name: '杭州-2'},
-                        {name: '杭州-3'},
-                        {name: '杭州-4'}
-                        ]
-                    },
-                    {
-                        name: '嘉兴',
-                        children: [
-                        {name: '嘉兴-1'},
-                        {name: '嘉兴-2'},
-                        {name: '嘉兴-3'},
-                        {name: '嘉兴-4'}
-                        ]
-                    },
-                    {name: '湖州'}
-                    ]
-                },{
-                    name: '福建',
-                    children: [
-                    {
-                        name: '福州',
-                        children: [
-                        {name: '福州-1'},
-                        {name: '福州-2'},
-                        {name: '福州-3'}
-                        ]
-                    },
-                    {name: '福建-2'},
-                    {name: '福建-3'}
-                    ]
-                }
-            ]
+            source: ajax()
         }
     },
     methods: {
